a10ad6cd9dfd2e9cfeb6cb789661f992323f2368
Mun1nm <joaoaveraldo@yahoo.com.br>
2024-05-21 13:41:04 -0300

    Adicionado a feature de adicionar ao carrinho

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,7 +7,7 @@ import { CartContext } from "../../context/cart.context"
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
 
     const toggleIsCartOpen = () => {
         setIsCartOpen(!isCartOpen);
@@ -16,7 +16,7 @@ const CartIcon = () => {
     return (
         <div className="cart-icon-container" onClick={toggleIsCartOpen}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{cartItemCount}</span>
         </div>
     )
 }
